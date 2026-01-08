48dca8b2800fffd4d9644ff6cedb048bb1a922e5
Huney YoGrrt 3 <aaronkebbell@gmail.com>
2026-01-08 21:22:32 +0000

    created a simple file for controlling the color

---
+++ Ui/ColorControlFile.py
@@ -0,0 +1,10 @@
+# todo [ ] add a way the change the theme
+class ColorControl:
+    def DarkBackGroundColor(self):
+        return "#001515"
+
+    def LightBackGroundColor(self):
+        return "#003033"
+
+    def HighLightColor(self):
+        return "#00becc"
