fc61dc6cbf53955678d30dbac47d4a90d90a1484
Huney YoGrrt 3 <aaronkebbell@gmail.com>
2026-01-08 21:21:29 +0000

    created a simple backing for the day events display

--- Ui/MainWindowFile.py
+++ Ui/MainWindowFile.py
@@ -1,7 +1,38 @@
+import tkinter as tk
+from Ui.ColorControlFile import ColorControl
+import math
+def RoundToSigN(x, sig=3):
+    return round(x, sig - int(math.floor(math.log10(abs(x)))) - 1)
+
 class CreateUiElimants():
     """docstring for CreateUiElimants"""
     def __init__(self):
         print ("imported CreateUiElimants")
+        self.CC = ColorControl() # this is the color control 
 
     def EventsToDay(self):
         print ("events ")
+
+    def BuildDayEvents(self, Window, MainCanvas):
+        LittleTempList = []
+        
+        # creates the back ground for day display
+        T1 = MainCanvas.create_rectangle(0, 0, 350, Window.winfo_height(), fill=self.CC.LightBackGroundColor(), outline=self.CC.HighLightColor())
+        LittleTempList.append(T1)
+        # T2 = MainCanvas.create_line(175, 0, 175, ScreenData.height, fill=self.CC.HighLightColor())
+        # LittleTempList.append(T2)
+        HourSepration = RoundToSigN(Window.winfo_height() / 24)-0.1
+        print (Window.winfo_height(), HourSepration)
+        offset = 0
+        for x in range(25):
+            # creates a line for each our in the day at a reguler interval
+            T3 = MainCanvas.create_line(10, offset, 340, offset, fill=self.CC.HighLightColor())
+            LittleTempList.append(T3)
+
+            # adds text to see what the hour is
+            # todo [ ] add am and pm back 
+            Hour = f"{x}"
+            if x == 13 or x > 13:
+                Hour = f"{x - 12}"
+            T4 = MainCanvas.create_text(175, offset-7, text=Hour, fill=self.CC.HighLightColor())
+            offset += HourSepration
--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from screeninfo import get_monitors
 import tkinter as tk
 
+from Ui.ColorControlFile import ColorControl
 from Ui.MainWindowFile import CreateUiElimants as Eliment
 
+import time
 class MainWindow(tk.Tk):
     """docstring for MainWindow"""
     def __init__(self):
         super().__init__() # this is what tels the class to use tk
-
+        self.CC = ColorControl() # this is the color control 
         self.Monitor = get_monitors()[0] # gets the second monitor
 
+        self.TBDOTTAR = [] # or TheBigDictOfThingsThatAreRenderd :)
         # creates the window and places it on the second screen
         self.geometry(f"+{self.Monitor.x}+{self.Monitor.y}")
         self.state("zoomed")
         
         # crates the canvas
-        self.CalinderCanvas = tk.Canvas(self, bg="#44626d")
+        self.CalinderCanvas = tk.Canvas(self, bg=self.CC.DarkBackGroundColor())
         self.CalinderCanvas.pack(fill="both", expand=True)
 
-        Eliment().EventsToDay() # creates the side bar to view the the events of today
+        self.after(10, self.OnStart)
         self.mainloop()
+
+    def OnStart(self):
+        DayEventsBGC = Eliment().BuildDayEvents(self, self.CalinderCanvas) # creates the side bar to view the the events of today
+        self.TBDOTTAR.append(DayEventsBGC)
 
 MainWindow()
