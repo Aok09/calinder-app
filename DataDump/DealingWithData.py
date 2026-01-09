15bd70ffac96193226b0053a6c6f3f694c167648
Huney YoGrrt 3 <aaronkebbell@gmail.com>
2026-01-09 16:17:48 +0000

    made a shitty data formatter

---
+++ DataDump/DealingWithData.py
@@ -0,0 +1,20 @@
+import json
+with open("UkHolidaysFile.json", "r") as File:
+    Holidays = json.load(File)
+
+DictOfHolidays = {}
+
+
+for area in Holidays:
+    for eventtype in Holidays[area]:
+        if type(Holidays[area][eventtype]) == list:
+            for event in Holidays[area][eventtype]:
+                DictOfHolidays[event["date"]] = event["title"]
+
+# for event in DictOfHolidays:
+    # print (event, DictOfHolidays[event])
+
+with open("UkHolidaysNewFile.json", "w") as File:
+    json.dump(DictOfHolidays, File)
+
+print(DictOfHolidays)
