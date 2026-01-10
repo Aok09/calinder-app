db1258e67ec5e7408643815fd20701062bf26a88
Huney YoGrrt 3 <aaronkebbell@gmail.com>
2026-01-10 00:22:39 +0000

    added a font crontol sctipt

---
+++ Ui/FontControlFile.py
@@ -0,0 +1,7 @@
+class FontControl():
+    def TitleFont(self):
+        TitleFont= ("KoHo", 75, "bold")
+        return TitleFont
+    def TimeFont(self):
+        TimeFont = ("KoHo", 25, "bold")
+        return TimeFont
